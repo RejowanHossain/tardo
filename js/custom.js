74099b900d71b2b2f4d0391497b3a069f95ab0b5
RejowanHossain <49896726+RejowanHossain@users.noreply.github.com>
2020-05-30 13:03:05 +0600

    added all the files

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$('.toggle_btn').on('click', function(e) {
+  $('.toggle-area').toggleClass("d-none"); //you can list several class names 
+  // e.preventDefault();
+});
+
+$('.emoji-btn').on('click', function(e) {
+  $('.emoji-box').toggleClass("d-none"); //you can list several class names 
+  // e.preventDefault();
+});
